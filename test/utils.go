0c06ab6bcdf20a2a4b064acc1e58c4bb94790b63
XciD <adrien@xcid.fr>
2019-10-18 10:50:03 +0200

    Add test utils package (#11)

---
+++ test/utils.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"github.com/XciD/loxone-ws"
+	"github.com/XciD/loxone-ws/events"
+)
+
+type FakeWebsocket struct {
+	hooks    map[string]func(*events.Event)
+	commands []string
+}
+
+func (l *FakeWebsocket) AddHook(uuid string, callback func(*events.Event)) {
+	l.hooks[uuid] = callback
+}
+func (l *FakeWebsocket) SendCommand(command string, class interface{}) (*loxone.Body, error) {
+	l.commands = append(l.commands, command)
+	return &loxone.Body{Code: 200}, nil
+}
+
+func (l *FakeWebsocket) Close() {
+
+}
+
+func (l *FakeWebsocket) RegisterEvents() error {
+	return nil
+}
+
+func (l *FakeWebsocket) PumpEvents(stop <-chan bool) {
+
+}
+
+func (l *FakeWebsocket) GetConfig() (*loxone.Config, error) {
+	return nil, nil
+}
+
+func (l *FakeWebsocket) TriggerEvent(uuid string, value float64) {
+	if hook, ok := l.hooks[uuid]; ok {
+		hook(&events.Event{Value: value})
+	}
+}
+
+func (l *FakeWebsocket) GetCommands() []string {
+	return l.commands
+}
